7ac16d72b8e55a9b2058a71ef235d7f9e3cd7a3d
rahulms123 <141030348+rahulms123@users.noreply.github.com>
2023-08-20 19:11:34 +0530

    PROGRAMMING ON METHODES

---
+++ Discount/src/Purchase.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Purchase {
+public static void checkDiscount(double purchaseAmount)
+{
+	if(purchaseAmount>100)
+	{
+		System.out.println("Discount Applicable");
+}
+}
+
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+	System.out.println("Enter Purchase Amount");
+	double purchaseAmount=scan.nextDouble();
+	checkDiscount(purchaseAmount);
+	}
+}
+
