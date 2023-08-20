81681e7f7cbc92356194c4e1e0b6fefc35622b51
rahulms123 <141030348+rahulms123@users.noreply.github.com>
2023-08-20 12:10:31 +0530

    PROGRAMS ON METHODS
    ALL PROGRAMS BELONGS TO CONCEPT CALLED METHODES

---
+++ 5-TheJourneyCalculater/src/JourneyCalculater.java
@@ -0,0 +1,8 @@
+
+public class JourneyCalculater {
+		public double calculateDistance(double speed, double time)
+		{
+		return speed*time;
+        }
+		}
+
---
+++ 6-TemperatureConverter/src/TemperatureConverter1.java
@@ -0,0 +1,8 @@
+
+public class TemperatureConverter1 {
+public double convertureFahrenheitToCelsius(double fahrenheit) {
+	double celcius=(fahrenheit-32)*5/9;
+	return celcius;
+	
+}
+}
